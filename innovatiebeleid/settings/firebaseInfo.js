fd1f094aa1453fd145a690eb155a1153766e375b
q-method <132446059+matswiefferkampioen23@users.noreply.github.com>
2023-05-03 13:11:39 +0200

    Add files via upload

---
+++ innovatiebeleid/settings/firebaseInfo.js
@@ -0,0 +1,14 @@
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyCA4NmdIkJnE1TkpEDZM_ZrejWguuNExDA",
+  authDomain: "ervaren-mobiliteitsarmoede.firebaseapp.com",
+  databaseURL: "https://ervaren-mobiliteitsarmoede-default-rtdb.firebaseio.com",
+  projectId: "ervaren-mobiliteitsarmoede",
+  storageBucket: "ervaren-mobiliteitsarmoede.appspot.com",
+  messagingSenderId: "222096647353",
+  appId: "1:222096647353:web:ecc0501370ebc3719b2768",
+  measurementId: "G-ZM52VY66JM"
+};
+firebase.initializeApp(config);
+var rootRef = firebase.database().ref();
+
